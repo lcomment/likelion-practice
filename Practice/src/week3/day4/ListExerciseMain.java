25028975fb592b24d59a212b4c2a32036253d159
lcomment <kohyunsuk98@gmail.com>
2022-10-06 09:27:07 +0900

    prac: list 실습

--- Practice/src/week3/day4/ListExerciseMain.java
+++ Practice/src/week3/day4/ListExerciseMain.java
@@ -1,7 +1,15 @@
 package week3.day4;
 
+import java.util.List;
+
 public class ListExerciseMain {
     public static void main(String[] args) {
+        ListExercise listExercise = new ListExercise();
 
+        List<String> students = listExercise.getStudents();
+        for (String student: students){
+            System.out.println(student);
+        }
+        System.out.println(students.size());
     }
 }
