8d774fd9991da54cb269801e3f21692dfe4d83c5
lcomment <kohyunsuk98@gmail.com>
2022-10-11 09:46:14 +0900

    자릿수 계산하기

---
+++ Practice/Practice/src/week4/day1/자릿수계산하기.java
@@ -0,0 +1,13 @@
+package week4.day1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 자릿수계산하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine().length());
+    }
+}
