110e2fecffcad4c2b5af127ba8bc706a03b430b8
lcomment <kohyunsuk98@gmail.com>
2022-10-26 00:26:40 +0900

    setting: 프로젝트 정리

--- Practice/Practice/src/week3/day3/NumberGenerator.java
+++
@@ -1,5 +0,0 @@
-package week3.day3;
-
-public interface NumberGenerator {
-    int generate(int num);
-}
--- Practice/Practice/src/week3/day3/RandomNumberGenerator.java
+++
@@ -1,8 +0,0 @@
-package week3.day3;
-
-public class RandomNumberGenerator implements NumberGenerator{
-    @Override
-    public int generate(int num) {
-        return (int) (Math.random() * num);
-    }
-}
