2fa561322098f3bb2e3b64687df07e147d663bc8
lcomment <kohyunsuk98@gmail.com>
2022-11-03 11:00:33 +0900

    프로그래머스 12921 소수 찾기

---
+++ algorithm/src/week6/quiz/Programmers_12921.java
@@ -0,0 +1,26 @@
+package week6.quiz;
+
+public class Programmers_12921 {
+    public int solution(int n) {
+        int answer = 0;
+        boolean[] prime = new boolean [n+1];
+        
+        for(int i=2; i<=n ; i++)
+            prime[i]=true;
+
+        int root = (int)Math.sqrt(n);
+
+        for(int i=2; i<=root; i++){
+            if(prime[i]){
+                for(int j=i; i*j<=n; j++)
+                    prime[i*j]=false;
+            }
+        }
+
+        for(int i =2; i<=n; i++) {
+            if(prime[i])
+                answer++;
+        }
+        return answer;
+    }
+}
