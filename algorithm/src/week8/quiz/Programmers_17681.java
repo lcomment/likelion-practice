97d13076b5de4f2ce0a6a2883d81e1b114108bdb
lcomment <kohyunsuk98@gmail.com>
2022-11-11 09:35:24 +0900

    Programmers 17681 비밀지도 (바이너리 메서드까지 구현)

--- algorithm/src/week8/quiz/Programmers_17681.java
+++ algorithm/src/week8/quiz/Programmers_17681.java
@@ -6,42 +6,51 @@ public class Programmers_17681 {
         String[] map2 = new String[n];
 
         for(int i=0 ; i<n ; i++){
-            String binaryNum1 = Integer.toBinaryString(arr1[i]);
-            String binaryNum2 = Integer.toBinaryString(arr2[i]);
+            // StringBuilder binaryNum1 = new StringBuilder(Integer.toBinaryString(arr1[i]));
+            // StringBuilder binaryNum2 = new StringBuilder(Integer.toBinaryString(arr2[i]));
+            StringBuilder binaryNum1 = new StringBuilder(toBinary(arr1[i]));
+            StringBuilder binaryNum2 = new StringBuilder(toBinary(arr2[i]));
 
             // n X n 맵으로 만들기
-            if(binaryNum1.length() < n){
-                while(true){
-                    binaryNum1 = "0" + binaryNum1;
-                    if(binaryNum1.length() == n)
-                        break;
-                }
-            } // if_binaryNum1
+            while(binaryNum1.length() < n){
+                binaryNum1.insert(0, 0);
+            }
 
             // n X n 맵으로 만들기
-            if(binaryNum2.length() < n){
-                while(true){
-                    binaryNum2 = "0" + binaryNum2;
-                    if(binaryNum2.length() == n)
-                        break;
-                }
-            } // if_binaryNum2
+            while(binaryNum2.length() < n){
+                binaryNum2.insert(0, 0);
+            }
 
-            map1[i] = binaryNum1;
-            map2[i] = binaryNum2;
+            map1[i] = binaryNum1.toString();
+            map2[i] = binaryNum2.toString();
         } // for_i
 
         String[] answer = new String[n];
 
         for(int i=0 ; i<n ; i++){
-            answer[i] = "";
+            StringBuilder sb = new StringBuilder();
+
             for(int j=0 ; j<n ; j++){
-                if(map1[i].charAt(j) == '1' || map2[i].charAt(j) == '1')
-                    answer[i] += "#";
-                else
-                    answer[i] += " ";
+                if(map1[i].charAt(j) == '1' || map2[i].charAt(j) == '1'){
+                    sb.append("#");
+                } else {
+                    sb.append(" ");
+                }
             } // for_j
+            answer[i] = sb.toString();
         } // for_i
         return answer;
+    }
+
+    // 10진수를 2진수로 만드는 메서드 
+    String toBinary(int num){
+        StringBuilder result = new StringBuilder();
+        while(num != 0){
+            int div = num % 2;
+            num /= 2;
+            result.append(div);
+        }
+
+        return result.reverse().toString();
     }
 }
